0494e643e87232822f4b27778cc3d2c2dd043bb5
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-19 21:07:51 +0530

    first commit

--- index.js
+++ index.js
@@ -42,17 +42,25 @@ app.get("/posts/new",(req,res) =>{
 
 app.post("/posts",(req,res) =>{
    let {username,content} = req.body;
-   posts.push({username,content});
+   let id = uuidv4(); 
+   posts.push({ id,username,content});
    res.redirect("/posts");
-
 });
 
 app.get("/posts/:id",(req,res) => {
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs",{post});
-   
-  
+});
+
+app.patch("/posts/:id",(req,res) =>{
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.send("Patch request working");
+
 });
 
 app.listen(port , () =>{
